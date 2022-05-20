5d0b0ef4d91a0541b7c0c3cf5a7633f21891ed57
manufarez <manufarez@gmail.com>
2022-05-20 12:16:42 -0500

    Changed colors

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,6 +11,15 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'asfe-blue': '#222e52',
+        'asfe-blue-dark': '#121D3D',
+        'asfe-green': '#429a86',
+        'asfe-green-light': '#bbd7c0',
+        'asfe-yellow': '#e9d883',
+        'asfe-red': '#771611',
+        'asfe-blue-light': '#0095b3'
+      },
     },
   },
   plugins: [
